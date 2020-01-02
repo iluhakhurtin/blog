346396850c16e58e13d2e24b3f8dd3a14c9a5e5a
Illia Khurtin <iluhakhurtin@gmail.com>
2020-01-03 08:00:19 +1100

    Enumerating photo files in a given folder

--- FileImport/FileImport/Program.cs
+++ FileImport/FileImport/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Npgsql;
+using System.Linq;
 
 namespace FileImport
 {
@@ -8,39 +9,59 @@ namespace FileImport
     {
         static void Main(string[] args)
         {
-            var filePath = GetFilePath();
-            while (!File.Exists(filePath))
+            string directoryPath = String.Empty;
+
+            while (!Directory.Exists(directoryPath))
             {
-                filePath = GetFilePath();
+                Console.WriteLine("Enter path to the directory with photos: ");
+                directoryPath = Console.ReadLine();
             }
 
-            Console.WriteLine("Enter server address: ");
-            var server = Console.ReadLine();
-
-            Console.WriteLine("Enter user: ");
-            var user = Console.ReadLine();
-
-            Console.WriteLine("Enter password: ");
-            var password = Console.ReadLine();
-
-            string connectionString = string.Format("Server={0};Port=5432;Database=Blog;User Id={1};Password={2};",
-                server, user, password);
-
-            var data = File.ReadAllBytes(filePath);
-            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            var photoFiles = Directory.EnumerateFiles(directoryPath).Where(fn => fn.EndsWith(".jpg") || fn.EndsWith(".JPG")).ToList();
+            if(photoFiles.Count > 0)
             {
-                string sql = @"UPDATE ""Files"" SET ""Data"" = :Data
-                                WHERE ""Id"" = '86c001a8-3975-8073-f1e3-067bb081d215';
-                            ";
-
-                using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                Console.WriteLine("The files to be processed:");
+                foreach (var photoFile in photoFiles)
                 {
-                    var dataParam = command.Parameters.AddWithValue("Data", data);
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
+                    Console.WriteLine(photoFile);
                 }
             }
+
+
+
+            //var filePath = GetFilePath();
+            //while (!File.Exists(filePath))
+            //{
+            //    filePath = GetFilePath();
+            //}
+
+            //Console.WriteLine("Enter server address: ");
+            //var server = Console.ReadLine();
+
+            //Console.WriteLine("Enter user: ");
+            //var user = Console.ReadLine();
+
+            //Console.WriteLine("Enter password: ");
+            //var password = Console.ReadLine();
+
+            //string connectionString = string.Format("Server={0};Port=5432;Database=Blog;User Id={1};Password={2};",
+            //    server, user, password);
+
+            //var data = File.ReadAllBytes(filePath);
+            //using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            //{
+            //    string sql = @"UPDATE ""Files"" SET ""Data"" = :Data
+            //                    WHERE ""Id"" = '86c001a8-3975-8073-f1e3-067bb081d215';
+            //                ";
+
+            //    using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+            //    {
+            //        var dataParam = command.Parameters.AddWithValue("Data", data);
+
+            //        connection.Open();
+            //        command.ExecuteNonQuery();
+            //    }
+            //}
         }
 
         private static string GetFilePath()
